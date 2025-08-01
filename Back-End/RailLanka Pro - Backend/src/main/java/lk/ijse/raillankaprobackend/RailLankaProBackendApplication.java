602d9544bab89d2cff283bf8048ad8b3009c49ff
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-01 20:53:19 +0530

    back-end initial commit

---
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/RailLankaProBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.raillankaprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailLankaProBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailLankaProBackendApplication.class, args);
+    }
+
+}
