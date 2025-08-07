25c844f0cbd0506206d0b7b676992820c00c93fc
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-07 18:27:32 +0530

    Add jwtUtil class for handling JWT token generation, validation, and extraction

---
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/util/jwtUtil.java
@@ -0,0 +1,56 @@
+package lk.ijse.raillankaprobackend.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project RailLanka Pro - Backend
+ * @github https://github.com/ManuthLakdiw
+ */
+@Component
+public class jwtUtil {
+
+    @Value("${jwt.expiration.time}")
+    private long expirationTime;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUserName(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
