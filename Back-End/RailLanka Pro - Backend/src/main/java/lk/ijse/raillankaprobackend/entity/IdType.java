e9cd9cbdd2927e6d6ce29028c3df9ba48a8d6df7
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-10 01:55:01 +0530

    Add enum classes for PassengerType, IdType, and SystemUserRole

---
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/entity/IdType.java
@@ -0,0 +1,12 @@
+package lk.ijse.raillankaprobackend.entity;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project RailLanka Pro - Backend
+ * @github https://github.com/ManuthLakdiw
+ */
+public enum IdType {
+    NIC,
+    PASSPORT,
+}
