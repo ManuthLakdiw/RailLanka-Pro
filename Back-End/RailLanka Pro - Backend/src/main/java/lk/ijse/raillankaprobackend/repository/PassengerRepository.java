369677aa20fe600e4b7d66628284e9b61083f5e9
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-10 01:56:05 +0530

    Add Passenger Repo,Service and User Repo,Service classes

---
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/repository/PassengerRepository.java
@@ -0,0 +1,19 @@
+package lk.ijse.raillankaprobackend.repository;
+
+import lk.ijse.raillankaprobackend.entity.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+/**
+ * @author manuthlakdiv
+ * @email manuthlakdiv2006.com
+ * @project RailLanka Pro - Backend
+ * @github https://github.com/ManuthLakdiw
+ */
+public interface PassengerRepository extends JpaRepository <Passenger,String> {
+
+    @Query(value = "SELECT passenger_id FROM passenger ORDER BY passenger_id DESC LIMIT 1", nativeQuery = true)
+    Optional<String> getLastPassengerId();
+}
