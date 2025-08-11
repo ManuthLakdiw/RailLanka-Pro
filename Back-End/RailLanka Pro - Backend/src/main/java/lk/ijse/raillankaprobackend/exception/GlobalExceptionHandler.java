865657b58b764837658d0ef7e21fa3a6ff979da1
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-12 01:32:10 +0530

    Handle additional exceptions in GlobalExceptionHandler for authentication and runtime errors

--- Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/exception/GlobalExceptionHandler.java
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package lk.ijse.raillankaprobackend.exception;
 
 import lk.ijse.raillankaprobackend.util.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -35,4 +37,35 @@ public class GlobalExceptionHandler {
                 null
         );
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiResponse<String> badCredentialExceptionHandler(BadCredentialsException ex){
+        return new ApiResponse<>(
+                401,
+                ex.getMessage(),
+                null
+        );
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiResponse<String> tokenExpiredExceptionHandler(TokenExpiredException ex){
+        return new ApiResponse<>(
+                401,
+                ex.getMessage(),
+                null
+        );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<String> runtimeExceptionHandler(RuntimeException ex){
+        return new ApiResponse<>(
+                500,
+                ex.getMessage(),
+                null
+        );
+    }
+
 }
