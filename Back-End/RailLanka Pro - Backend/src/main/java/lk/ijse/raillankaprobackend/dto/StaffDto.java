6388b423b1b01e6f091caff45ace993148bd06fb
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-08-20 19:32:33 +0530

    refactor StaffDto entity
    - Replaced `name` with `firstname` and `lastname` for improved clarity.
    - Added new fields: `address`, `dob`, `railwayStation`, and `yearsOfExperience`.

--- Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/dto/StaffDto.java
+++ Back-End/RailLanka Pro - Backend/src/main/java/lk/ijse/raillankaprobackend/dto/StaffDto.java
@@ -2,6 +2,8 @@ package lk.ijse.raillankaprobackend.dto;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 /**
  * @author manuthlakdiv
  * @email manuthlakdiv2006.com
@@ -16,12 +18,16 @@ import lombok.*;
 @Builder
 public class StaffDto {
     private String id;
-    private String title;
-    private String name;
+    private String firstname;
+    private String lastname;
     private String userName;
     private String password;
     private String idNumber;
     private String phoneNumber;
     private String email;
+    private String address;
+    private LocalDate dob;
+    private String railwayStation;
+    private int yearsOfExperience;
     private boolean active;
 }
